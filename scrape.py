a03785a011dfa63a2120b9a2fe03fd16339f689d
HarryRichard08 <75106918+HarryRichard08@users.noreply.github.com>
2023-08-24 17:15:17 +0530

    Create scrape.py

---
+++ scrape.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+# URL of the webpage to scrape
+url = "https://www.example-news-site.com"
+
+# Send an HTTP GET request to the URL
+response = requests.get(url)
+
+# Parse the HTML content of the page using Beautiful Soup
+soup = BeautifulSoup(response.content, "html.parser")
+
+# Find all the article title elements on the page
+article_titles = soup.find_all("h2", class_="article-title")
+
+# Loop through the article title elements and print the titles
+for title_element in article_titles:
+    title_text = title_element.get_text()
+    print("Article Title:", title_text)
