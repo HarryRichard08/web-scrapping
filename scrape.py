966a0afb6b2f89921f00374135b6cd76c369b72e
HarryRichard08 <75106918+HarryRichard08@users.noreply.github.com>
2023-08-30 08:30:46 +0530

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -7,6 +7,8 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import csv
 
+#demo 
+
 def scrape_website():
     # Make a request to the website
     r = requests.get("https://books.toscrape.com")
